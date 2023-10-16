0200d93ccb04f251577f621e22c90e8c8295c51a
Vitaly Likhachev <vitaly99878@gmail.com>
2023-10-16 17:19:09 +0300

    DS - test commit

--- Desigm Patterns/Creational/Abstract Factory.ts
+++ Desigm Patterns/Creational/Abstract Factory.ts
@@ -17,13 +17,89 @@ interface AudioApi {
   searchByLyric: (anyPartOfLyric: string) => AudioTrack[];
 }
 
-class NormalMovieApiProvider implements MovieApi { /* Implementation */ }
+class NormalMovieApiProvider implements MovieApi {
+  searchByActors(actors: string[]): Movie[] {
+    return [];
+  }
 
-class NormalAudioApiProvider implements AudioApi { /* Implementation */ }
+  searchByAwards(awards: string[]): Movie[] {
+    return [];
+  }
 
-class PremiumMovieApiProvider implements MovieApi { /* Implementation */ }
+  searchByDirector(director: string): Movie[] {
+    return [];
+  }
 
-class PremiumAudioApiProvider implements AudioApi { /* Implementation */ }
+  searchByName(name: string): Movie[] {
+    return [];
+  }
+
+  searchByReleaseDate(releaseDate: Date): Movie[] {
+    return [];
+  } /* Implementation */ }
+
+class NormalAudioApiProvider implements AudioApi {
+  searchByArtist(artist: string): AudioTrack[] {
+    return [];
+  }
+
+  searchByGenre(genre: string): AudioTrack[] {
+    return [];
+  }
+
+  searchByLyric(anyPartOfLyric: string): AudioTrack[] {
+    return [];
+  }
+
+  searchByMood(mood: string): AudioTrack[] {
+    return [];
+  }
+
+  searchByName(name: string): AudioTrack[] {
+    return [];
+  } /* Implementation */ }
+
+class PremiumMovieApiProvider implements MovieApi {
+  searchByActors(actors: string[]): Movie[] {
+    return [];
+  }
+
+  searchByAwards(awards: string[]): Movie[] {
+    return [];
+  }
+
+  searchByDirector(director: string): Movie[] {
+    return [];
+  }
+
+  searchByName(name: string): Movie[] {
+    return [];
+  }
+
+  searchByReleaseDate(releaseDate: Date): Movie[] {
+    return [];
+  } /* Implementation */ }
+
+class PremiumAudioApiProvider implements AudioApi {
+  searchByArtist(artist: string): AudioTrack[] {
+    return [];
+  }
+
+  searchByGenre(genre: string): AudioTrack[] {
+    return [];
+  }
+
+  searchByLyric(anyPartOfLyric: string): AudioTrack[] {
+    return [];
+  }
+
+  searchByMood(mood: string): AudioTrack[] {
+    return [];
+  }
+
+  searchByName(name: string): AudioTrack[] {
+    return [];
+  } /* Implementation */ }
 
 interface ApiProviderFactory {
   createMovieApiProvider: () => MovieApi;
